163bf39bf73ac03b64ff8503415295306d91ff8a
Kelvin Forteta <kelvin.forteta@gmail.com>
2019-09-18 20:11:11 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(DevFestWarri());
+
+class DevFestWarri extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
